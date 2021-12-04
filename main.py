0f2f9ccfd3fefef1dd18ed61a16e1f516e5285a0
Shaaman331 <tarcisio2010souza@gmail.com>
2021-12-04 20:27:20 -0300

    Integrando valores com os botões na tela com a função lambda.

--- main.py
+++ main.py
@@ -47,42 +47,42 @@ b_1 = Button(frame_corpo, text= 'C', width=11, height=2, bg= cor4,font=('Ivy 13 
 b_1.place(x=0, y=0) #executando tela, localização da tela
 b_2 = Button(frame_corpo, command= lambda :entrar_valores('%'), text= '%', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão módulo
 b_2.place(x=118, y=0) #executando tela, localização da tela, integrando valores com a função lambda 
-b_3 = Button(frame_corpo, text= '/', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão disão
-b_3.place(x=177, y=0) #executando tela, localização da tela
+b_3 = Button(frame_corpo, command= lambda :entrar_valores('/'),text= '/', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão disão
+b_3.place(x=177, y=0) #executando tela, localização da tela, integrando valores com a função lambda
 
-b_4 = Button(frame_corpo, text= '7', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão sete
-b_4.place(x=0, y=52) #executando tela, localização da tela
-b_5 = Button(frame_corpo, text= '8', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão oito
-b_5.place(x=59, y=52) #executando tela, localização da tela
-b_6 = Button(frame_corpo, text= '9', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão nove
-b_6.place(x=118, y=52) #executando tela, localização da tela
-b_7 = Button(frame_corpo, text= '*', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão multiplicação
-b_7.place(x=177, y=52) #executando tela, localização da tela
+b_4 = Button(frame_corpo, command= lambda :entrar_valores('7'),text= '7', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão sete
+b_4.place(x=0, y=52) #executando tela, localização da tela, integrando valores com a função lambda
+b_5 = Button(frame_corpo, command= lambda :entrar_valores('8'),text= '8', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão oito
+b_5.place(x=59, y=52) #executando tela, localização da tela, integrando valores com a função lambda
+b_6 = Button(frame_corpo, command= lambda :entrar_valores('9'),text= '9', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão nove
+b_6.place(x=118, y=52) #executando tela, localização da tela, integrando valores com função lambda
+b_7 = Button(frame_corpo, command= lambda :entrar_valores('*'),text= '*', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão multiplicação
+b_7.place(x=177, y=52) #executando tela, localização da tela, integrando valores com a função lambda
 
-b_8 = Button(frame_corpo, text= '4', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão quatro
-b_8.place(x=0, y=104) #executando tela, localização da tela
-b_9 = Button(frame_corpo, text= '5', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão cinco
-b_9.place(x=59, y=104) #executando tela, localização da tela
-b_10 = Button(frame_corpo, text= '6', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão seis
-b_10.place(x=118, y=104) #executando tela, localização da tela
-b_11 = Button(frame_corpo, text= '-', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão menos
-b_11.place(x=177, y=104) #executaando tela, localização da tela
+b_8 = Button(frame_corpo, command= lambda :entrar_valores('4'),text= '4', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão quatro
+b_8.place(x=0, y=104) #executando tela, localização da tela, integrando valores com a função lambda
+b_9 = Button(frame_corpo, command= lambda :entrar_valores('5'),text= '5', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão cinco
+b_9.place(x=59, y=104) #executando tela, localização da tela, integrando valores com a função lambda
+b_10 = Button(frame_corpo, command= lambda :entrar_valores('6'),text= '6', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão seis
+b_10.place(x=118, y=104) #executando tela, localização da tela, integrando valores com a função lambda
+b_11 = Button(frame_corpo, command= lambda :entrar_valores('-'),text= '-', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão menos
+b_11.place(x=177, y=104) #executaando tela, localização da tela, integrando valores com a função lambda
 
-b_12 = Button(frame_corpo, text= '1', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão um
-b_12.place(x=0, y=156) #executando tela, localização da tela
-b_13 = Button(frame_corpo, text= '2', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão dois
-b_13.place(x=59, y=156) #executando tela, localização da tela
-b_14 = Button(frame_corpo, text= '3', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão trẽs
-b_14.place(x=118, y=156) #executando tela, localização da tela
-b_15 = Button(frame_corpo, text= '+', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão soma
-b_15.place(x=177, y=156) #executando tela, localização da tela
+b_12 = Button(frame_corpo, command= lambda :entrar_valores('1'),text= '1', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão um
+b_12.place(x=0, y=156) #executando tela, localização da tela, integrando valores com a função lambda
+b_13 = Button(frame_corpo, command= lambda :entrar_valores('2'),text= '2', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão dois
+b_13.place(x=59, y=156) #executando tela, localização da tela, integrando valores com a função lambda
+b_14 = Button(frame_corpo, command= lambda :entrar_valores('3'),text= '3', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão trẽs
+b_14.place(x=118, y=156) #executando tela, localização da tela, integrando valores com a função lambda
+b_15 = Button(frame_corpo, command= lambda :entrar_valores('+'),text= '+', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão soma
+b_15.place(x=177, y=156) #executando tela, localização da tela, integrando valores com a função lambda
 
-b_16 = Button(frame_corpo, text= '0', width=11, height=2, bg= cor4,font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE) #botão zero
-b_16.place(x=0, y=208) #executando tela, localização da tela
-b_17 = Button(frame_corpo, text= '.', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão ponto
-b_17.place(x=118, y=208) #executando tela, localização da tela
-b_18 = Button(frame_corpo, text= '=', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão igualdade
-b_18.place(x=177, y=208) #executando tela, localização da tela
+b_16 = Button(frame_corpo, command= lambda :entrar_valores('0'),text= '0', width=11, height=2, bg= cor4,font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE) #botão zero
+b_16.place(x=0, y=208) #executando tela, localização da tela, integrando valores com a função lambda
+b_17 = Button(frame_corpo, command= lambda :entrar_valores('.'),text= '.', width=5, height=2, bg= cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief= RIDGE) #botão ponto
+b_17.place(x=118, y=208) #executando tela, localização da tela, integrando valores com a função lambda
+b_18 = Button(frame_corpo, command= lambda :entrar_valores('%'),text= '=', width=5, height=2, bg= cor5, fg=cor3,font=('Ivy 13 bold'), relief= RAISED, overrelief= RIDGE) #botão igualdade
+b_18.place(x=177, y=208) #executando tela, localização da tela, integrando valores com a função lambda
 
 
 janela.mainloop() #Executar janela
